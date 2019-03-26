789be8e60c4ef1fe86281ad1f608f6cf139f7d67
SG <sgng1@e.ntu.edu.sg>
2019-03-26 09:07:19 +0000

    installed react-testing-library and jest-dom as dependencies instead

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import { render } from "react-testing-library";
 import "jest-dom/extend-expect";
 import App from "./App";
 
-describe("The index component", () => {
-  it("index component should render text initial state", () => {
+describe("The App component", () => {
+  it("App component should render text initial state", () => {
     const { getByText } = render(<App />);
     expect(getByText("initial state")).toBeInTheDocument();
   });
