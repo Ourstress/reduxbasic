16b11861180bd0f5b19f1ce3a56ecfec969e1bf1
SG <sgng1@e.ntu.edu.sg>
2019-03-27 07:34:57 +0000

    refined test case

--- src/App.test.js
+++ src/App.test.js
@@ -9,9 +9,13 @@ describe("The App component", () => {
   it("App component should hide jokeAnswer when hide joke answer button clicked", () => {
     const { getByText } = render(<App />);
     fireEvent.click(getByText(/Click to reveal joke answer/i));
+    const jokeAnswer = getByText(
+      /Because it saw something exciting on the other side/i
+    );
+    expect(jokeAnswer).toBeInTheDocument();
     fireEvent.click(getByText(/Click to hide joke answer/i));
-    expect(
-      getByText(/Because it saw something exciting on the other side/i)
-    ).not.toBeInTheDocument();
+    expect(jokeAnswer).not.toHaveTextContent(
+      /Because it saw something exciting on the other side/i
+    );
   });
 });
